fe2b2d8579ecd8c842979aec04d1fc991eeb53e7
hdgehog <hedgehogagaga@gmail.com>
2026-05-15 19:40:22 +0300

    add func delStudent

--- Academy/function.h
+++ Academy/function.h
@@ -6,13 +6,13 @@ enum MENU {
 	SEARCH_BY_NAME,
 	SEARCH_BY_ID,
 	SORT_BY_NAME,
+	SORT_BY_ID,
 	SORT_BY_AVERAGE,
+	DEL_STUDENT,
 	ADD_STUDENT
 };
 
 Student createStudent(){
-	cin.ignore();
-
 	Student std{};
 	static uint id = 100;
 	std.id = id++;
@@ -70,26 +70,25 @@ void showListOfStudents(Student*& students, uint size) {
 	}
 }
 
-void searchById(Student*& students, uint size, uint id) {
+uint searchId(Student*& students, uint size, uint id) {
 	for (uint i = 0; i < size; i++) {
 		if (students[i].id == id) {
-			showStudent(students[i]);
-			return;
+			return i;
 		}
 	}
 	cout << "Not found" << endl;
+	return size;
 }
 
 void searchByIdAction(Student*& students, uint size) {
-	cin.ignore();
 	uint input_int;
-	cout << "Enter id(0 - Exit): ";
-	cin >> input_int;
-	while (input_int != 0) {
-		searchById(students, size, input_int);
+	do {
 		cout << "Enter id(0 - Exit): ";
 		cin >> input_int;
-	}
+		if (input_int != 0 && searchId(students, size, input_int) != size) {
+			showStudent(students[searchId(students, size, input_int)]);
+		}
+	} while (input_int != 0);
 }
 
 void searchByName(Student*& students, uint size, char* name) {
@@ -117,15 +116,25 @@ void searchByNameAction(Student*& students, uint size) {
 	}
 }
 
-void sorting(Student*& students, uint size,void(*sort)(Student* students, uint size)) {
-	sort(students, size);
-
-}
+//void sorting(Student*& students, uint size,void(*sort)(Student* students, uint size)) {
+//	sort(students, size);
+//
+//}
 
 void sortByName(Student*& students, uint size) {
 	for (uint i = 0; i < size - 1; i++) {
 		for (uint j = 0; j < size - 1 - i; j++) {
-			if (strcmp(students[j].name, students[j+1].name) > 0) {
+			if (strcmp(students[j].name, students[j + 1].name) > 0) {
+				swap(students[j], students[j + 1]);
+			}
+		}
+	}
+}
+
+void sortById(Student*& students, uint size) {
+	for (uint i = 0; i < size - 1; i++) {
+		for (uint j = 0; j < size - 1 - i; j++) {
+			if (students[j].id > students[j + 1].id) {
 				swap(students[j], students[j + 1]);
 			}
 		}
@@ -153,6 +162,33 @@ void addStudent(Student*& students, uint& size)
 	size++;
 	delete[] students;
 	students = students_upd;
+	showListOfStudents(students, size);
+}
+
+void delStudent(Student*& students, uint& size)
+{
+	if (size-1 == 0) { 
+		delete[] students;
+		students = nullptr;
+		size--;
+		return; 
+	}
+	uint input_int;
+	cout << "Enter id: ";
+	cin >> input_int;
+	if (searchId(students, size, input_int) != size) {
+		uint idx = searchId(students, size, input_int);
+		Student* students_upd = new Student[size - 1];
+		for (int i = 0; i < size-1; i++)
+		{
+			students_upd[i] = (idx != i) ? (students[i]) : (students[size - 1]);
+		}
+		size--;
+		delete[] students;
+		students = students_upd;
+		sortById(students, size);
+	}
+	showListOfStudents(students, size);
 }
 
 void action() {
@@ -167,11 +203,14 @@ void action() {
 			<< ", Search by name - " << SEARCH_BY_NAME
 			<< ", Search by ID - " << SEARCH_BY_ID
 			<< ", Sort by name - " << SORT_BY_NAME
+			<< ", Sort by id - " << SORT_BY_ID
 			<< ", Sort by average - " << SORT_BY_AVERAGE
+			<< ", Delete a student - " << DEL_STUDENT
 			<< ", Add a student - " << ADD_STUDENT
 			<< " )" << endl;
 		
 		cin >> input_int;
+		cin.ignore();
 		switch (input_int) {
 		case EXIT:
 			clear(students, size);
@@ -197,9 +236,19 @@ void action() {
 			sortByName(students, size);
 			cout << "\n";
 			break;
+		case SORT_BY_ID:
+			if (size == 0) { break; }
+			sortById(students, size);
+			cout << "\n";
+			break;
 		case SORT_BY_AVERAGE:
 			if (size == 0) { break; }
 			sortByAverage(students, size);
+			cout << "\n";
+			break;
+		case DEL_STUDENT:
+			if (size == 0) { break; }
+			delStudent(students, size);
 			cout << "\n";
 			break;
 		case ADD_STUDENT:
@@ -210,6 +259,6 @@ void action() {
 			cout << "Error. Try again" << endl;
 			break;
 		}
-		cin.ignore();
+		
 	}
 }
